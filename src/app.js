7e0d63b9d72c890b831fc2391037605c7f31605e
Ismael Costa <ismaelirc@gmail.com>
2024-05-27 13:08:15 -0300

    Using separeted filed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import conecta from "./config/dbConnect.js";
-import livro from './models/Livro.js';
+import routes from './routes/index.js'
 
 const conexao = await conecta();
 
@@ -13,37 +13,6 @@ conexao.once("open", () => {
 })
 
 const app = express();
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.status(200).send("Curso de NodeJS");
-});
-
-app.get('/livros/:id', (req,res) => {
-    const index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index]);
-});
-
-app.post('/livros', (req, res) => {
-    livros.push(req.body);
-    res.status(201).json(livros);
-});
-
-app.put('/livros/:id', (req, res) => {
-    const index = buscaLivro(req.params.id);
-    
-    livros[index].titulo = req.body.titulo;
-
-    res.status(200).json(livros);
-});
-
-app.delete('/livros/:id', (req,res) => {
-    
-    const index = buscaLivro(req.params.id);
-    livros.splice(index,1);
-
-    res.status(200).json(livros);
-})
-
+routes(app);
 
 export default app;
--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -3,9 +3,65 @@ import livro from '../models/Livro.js';
 class LivroController{
 
     static async listarLivros(req, res){
-        const listaLivros = await livro.find({});
+        try {
+            const listaLivros = await livro.find({});
     
-        res.status(200).json(listaLivros);
+            res.status(200).json(listaLivros);            
+        } catch (error) {
+            res.status(500).json({'message': `${error.message} - Falha ao listar livro`});
+        }
+        
+    }
+
+    static async listarLivroPorId(req, res){
+        try {
+
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id);
+    
+            res.status(200).json(livroEncontrado);            
+        } catch (error) {
+
+            res.status(500).json({'message': `${error.message} - Falha ao buscar livro por ID`});
+        }
+        
+    }
+
+    static async atualizarLivro(req, res){
+        try {
+
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+    
+            res.status(200).json({'message': 'Livro atualizado'});            
+        } catch (error) {
+            
+            res.status(500).json({'message': `${error.message} - Falha ao atualizar livro`});
+        }
+        
+    }
+
+    static async cadastrarLivro(req, res){
+        try{
+            const novoLivro = await livro.create(req.body);
+            
+            res.status(201).json(novoLivro);
+        }catch(error){
+            res.status(500).json({'message': `${error.message} - Falha ao cadastrar livro`});
+        }
+        
+    }
+
+    static async deletarLivros(req,res){
+        try{
+
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id);
+            res.status(201).json({'message': 'Livro deletado'});
+
+        }catch(error){
+            res.status(500).json({'message': `${error.message} - Falha ao deletar livro`});
+        }
     }
 
 };
